2cbf4b54cbb82902ad141ec350a31ff7898a558b
mppfiles <mppfiles@users.noreply.github.com>
2019-11-20 08:50:33 -0300

    Update form.js
    actualizo handler para trabajar con errores json

--- src/main/webapp/js/form.js
+++ src/main/webapp/js/form.js
@@ -18,11 +18,23 @@ $(function () {
             $errores.html("").hide();
             window.top.location = "/ej_ajax/contacto/gracias";
         }).fail(function (xhr, status, error) {
-            if (xhr.status === 400) {
-                $errores.html(xhr.responseText).show();
-            } else {
-                alert("Ha ocurrido un error: " + error);
+            var data = {"global": "Ocurrió un error en la aplicación: " + error};
+            if(xhr.status === 400) {
+                try {
+                    data = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    data = {"global": "No se pudo procesar la respuesta del servidor."};
+                }
             }
+            
+            var $ul_errores = $("<ul></ul>");
+            $.each(data, function(key, value) {
+                var $li = $("<li></li>");
+                $li.text(key + ": "+ value);
+                $ul_errores.append($li);
+            });            
+            $errores.append($ul_errores).show();
+            
         }).always(function () {
             $cargando.hide();
         });
