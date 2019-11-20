4932eb7526234e0fd4c86b772e5e23220a0a767c
Martin P. <udc@udc-HP-ProBook-4520s>
2019-11-20 18:20:23 -0300

    chau TomcatHelper

--- src/main/java/app/config/AppBootstrap.java
+++ src/main/java/app/config/AppBootstrap.java
@@ -1,6 +1,7 @@
 package app.config;
 
-import app.util.helpers.TomcatHelper;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.javalite.activeweb.AppContext;
 import org.javalite.activeweb.Bootstrap;
@@ -16,11 +17,15 @@ public class AppBootstrap extends Bootstrap {
 
     private void setearEntornoTomcat(AppContext ac) {
         try {
-            ac.set("recaptcha_public_key", (String) TomcatHelper.getVariable("recaptcha_public_key"));
-            ac.set("recaptcha_private_key", (String) TomcatHelper.getVariable("recaptcha_private_key"));
-            ac.set("usuario_correo", (String) TomcatHelper.getVariable("usuario_correo"));
-            ac.set("password_correo", (String) TomcatHelper.getVariable("password_correo"));
-            ac.set("jwt_private_key", (String) TomcatHelper.getVariable("jwt_private_key"));
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            
+            ac.set("recaptcha_url", (String) envCtx.lookup("recaptcha_url"));
+            ac.set("recaptcha_public_key", (String) envCtx.lookup("recaptcha_public_key"));
+            ac.set("recaptcha_private_key", (String) envCtx.lookup("recaptcha_private_key"));
+            ac.set("usuario_correo", (String) envCtx.lookup("usuario_correo"));
+            ac.set("password_correo", (String) envCtx.lookup("password_correo"));
+            ac.set("jwt_private_key", (String) envCtx.lookup("jwt_private_key"));
         } catch (NamingException ex) {
             throw new RuntimeException(ex.getMessage(), ex);
         }
